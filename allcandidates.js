8782a683d376e591ed4257b965e955cf70f64442
jieqianzhang <zhang.jieqian@gmail.com>
2015-10-27 10:24:41 -0700

    version2

---
+++ allcandidates.js
@@ -0,0 +1,308 @@
+var allcandidates = {
+ "name": "GOP Debate - CNBC",
+ "rank": "",
+ "size":100,
+ "children": [
+  {
+   "name": "Donald Trump",
+   "rank": "1",
+   "size":100,
+   "children": [
+    {"name": "Ben Carson", "rank": "1", "size":100},
+    {"name": "Carly Fiorina", "rank": "2", "size":100},
+    {"name": "Jeb Bush", "rank": "3","size":100},
+    {"name": "Marco Rubio", "rank": "4", "size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie","rank": "7", "size":100},
+    {"name": "Mike Huckabee", "rank": "8","size":100},
+    {"name": "Bobby Jindal", "rank": "9","size":100},
+    {"name": "John Kasich", "rank": "10","size":100},
+    {"name": "Lindsey Graham","rank": "11", "size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+   ]
+  },
+  {
+   "name": "Ben Carson",
+   "rank": "2",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1", "size":100},
+    {"name": "Carly Fiorina","rank": "2", "size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Jeb Bush", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "Mike Huckabee", "rank": "8","size":100},
+    {"name": "John Kasich", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+    
+   ]
+  },
+  {
+   "name": "Carly Fiorina",
+   "rank": "3",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1","size":100},
+    {"name": "Ben Carson", "rank": "2","size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Jeb Bush", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "John Kasich", "rank": "8","size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Lindsey Graham", "rank": "10","size":100},
+    {"name": "Bobby Jindal", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "Jeb Bush",
+   "rank": "4",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1","size":100},
+    {"name": "Carly Fiorina", "rank": "2","size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "John Kasich","rank": "8", "size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "Marco Rubio",
+   "rank": "5",
+   "size":100,
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100},
+    {"name": "Carly Fiorina","rank": "2", "size":100},
+    {"name": "Donald Trump", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Jeb Bush", "rank": "5","size":100},
+    {"name": "Rand Paul","rank": "6", "size":100},
+    {"name": "Chris Christie","rank": "7", "size":100},
+    {"name": "John Kasich","rank": "8", "size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+   ]
+  },
+  {
+   "name": "Ted Cruz",
+   "rank": "6",
+   "size":100,
+   "children": [
+    {"name": "Marco Rubio", "rank": "1","size":100},
+    {"name": "Ben Carson", "rank": "2","size":100},
+    {"name": "Donald Trump", "rank": "3","size":100},
+    {"name": "Carly Fiorina", "rank": "4","size":100},
+    {"name": "Rand Paul", "rank": "5","size":100},
+    {"name": "Jeb Bush", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "Mike Huckabee", "rank": "8","size":100},
+    {"name": "John Kasich", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Rick Santorum", "rank": "11","size":100},
+    {"name": "Lindsey Graham", "rank": "12","size":100}
+    
+   ]
+  },
+  {
+   "name": "Rand Paul",
+   "rank": "7",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1","size":100},
+    {"name": "Ted Cruz", "rank": "2", "size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Carly Fiorina", "rank": "5","size":100},
+    {"name": "Jeb Bush", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "Mike Huckabee", "rank": "8", "size":100},
+    {"name": "John Kasich", "rank": "9","size":100},
+    {"name": "Rick Santorum","rank": "10", "size":100},
+    {"name": "Bobby Jindal", "rank": "11","size":100},
+    {"name": "Lindsey Graham", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13", "size":100},
+    {"name": "Jim Gilmore", "rank": "14","size":100}
+   ]
+  },
+  {
+   "name": "Chris Christie",
+   "rank": "8",
+   "size":100,
+   "children": [
+    {"name": "Carly Fiorina", "rank": "1","size":100},
+    {"name": "Donald Trump", "rank": "2","size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Jeb Bush", "rank": "5","size":100},
+    {"name": "Ted Cruz", "rank": "6","size":100},
+    {"name": "Rand Paul", "rank": "7","size":100},
+    {"name": "John Kasich","rank": "8", "size":100},
+    {"name": "Mike Huckabee","rank": "9", "size":100},
+    {"name": "Bobby Jindal","rank": "10", "size":100},
+    {"name": "Lindsey Graham","rank": "11", "size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "Mike Huckabee",
+   "rank": "9",
+   "size":100,
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100},
+    {"name": "Ben Carson", "rank": "2","size":100},
+    {"name": "Carly Fiorina", "rank": "3","size":100},
+    {"name": "Marco Rubio", "rank": "4","size":100},
+    {"name": "Rand Paul", "rank": "5","size":100},
+    {"name": "Donald Trump", "rank": "6","size":100},
+    {"name": "Jeb Bush", "rank": "7","size":100},
+    {"name": "Chris Christie", "rank": "8","size":100},
+    {"name": "John Kasich", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Rick Santorum", "rank": "11","size":100},
+    {"name": "Jim Gilmore", "rank": "12","size":100}
+   ]
+  },
+  {
+   "name": "John Kasich",
+   "rank": "10",
+   "size":100,
+   "children": [
+    {"name": "Carly Fiorina","rank": "1", "size":100},
+    {"name": "Marco Rubio", "rank": "2","size":100},
+    {"name": "Ted Cruz", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Rand Paul", "rank": "5","size":100},
+    {"name": "Jeb Bush", "rank": "6","size":100},
+    {"name": "Donald Trump","rank": "7","size":100},
+    {"name": "Chris Christie","rank": "8", "size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham","rank": "11", "size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100},
+    {"name": "Jim Gilmore", "rank": "14","size":100}
+   ]
+  },
+  {
+   "name": "Lindsey Graham",
+   "rank": "11",
+   "size":100,
+   "children": [
+    {"name": "Bobby Jindal","rank": "1", "size":100},
+    {"name": "Rick Santorum", "rank": "2","size":100},
+    {"name": "Carly Fiorina","rank": "3", "size":100},
+    {"name": "Donald Trump", "rank": "4","size":100},
+    {"name": "George Pataki", "rank": "5","size":100},
+    {"name": "Ted Cruz", "rank": "6","size":100},
+    {"name": "Jeb Bush", "rank": "7","size":100},
+    {"name": "Ben Carson", "rank": "8","size":100},
+    {"name": "Marco Rubio", "rank": "9","size":100},
+    {"name": "Chris Christie","rank": "10", "size":100},
+    {"name": "Rand Paul", "rank": "11","size":100},
+    {"name": "John Kasich", "rank": "12","size":100},
+    {"name": "Mike Huckabee", "rank": "13","size":100},
+    {"name": "Jim Gilmore", "rank": "14","size":100}
+   ]
+  },
+  {
+   "name": "Bobby Jindal",
+   "rank": "12",
+   "size":100,
+   "children": [
+    {"name": "Lindsey Graham", "rank": "1","size":100},
+    {"name": "Donald Trump", "rank": "2","size":100},
+    {"name": "Ben Carson", "rank": "3","size":100},
+    {"name": "Rick Santorum", "rank": "4","size":100},
+    {"name": "Carly Fiorina", "rank": "5","size":100},
+    {"name": "George Pataki","rank": "6", "size":100},
+    {"name": "Marco Rubio", "rank": "7","size":100},
+    {"name": "Ted Cruz", "rank": "8","size":100},
+    {"name": "Rand Paul", "rank": "9","size":100},
+    {"name": "Jeb Bush", "rank": "10","size":100},
+    {"name": "Mike Huckabee", "rank": "11","size":100},
+    {"name": "Chris Christie", "rank": "12","size":100},
+    {"name": "John Kasich", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "George Pataki",
+   "rank": "13",
+   "size":100,
+   "children": [
+    {"name": "Lindsey Graham", "rank": "1","size":100},
+    {"name": "Bobby Jindal", "rank": "2","size":100},
+    {"name": "Rick Santorum", "rank": "3","size":100},
+    {"name": "Rand Paul", "rank": "4","size":100},
+    {"name": "John Kasich", "rank": "5","size":100},
+    {"name": "Jim Gilmore", "rank": "6","size":100},
+    {"name": "Carly Fiorina", "rank": "7","size":100},
+    {"name": "Chris Christie", "rank": "8","size":100},
+    {"name": "Donald Trump","rank": "9", "size":100},
+    {"name": "Marco Rubio", "rank": "10","size":100},
+    {"name": "Jeb Bush", "rank": "11","size":100},
+    {"name": "Mike Huckabee", "rank": "12","size":100}
+   ]
+  },
+  {
+   "name": "Rick Santorum",
+   "rank": "14",
+   "size":100,
+   "children": [
+    {"name": "Lindsey Graham", "rank": "1","size":100},
+    {"name": "Bobby Jindal", "rank": "2","size":100},
+    {"name": "George Pataki", "rank": "3","size":100},
+    {"name": "Marco Rubio", "rank": "4","size":100},
+    {"name": "Carly Fiorina", "rank": "5","size":100},
+    {"name": "Ted Cruz", "rank": "6","size":100},
+    {"name": "Rand Paul","rank": "7", "size":100},
+    {"name": "Donald Trump", "rank": "8","size":100},
+    {"name": "Ben Carson", "rank": "9","size":100},
+    {"name": "Mike Huckabee", "rank": "10","size":100},
+    {"name": "Jeb Bush", "rank": "11","size":100},
+    {"name": "Chris Christie", "rank": "12","size":100},
+    {"name": "Jim Gilmore", "rank": "13","size":100},
+    {"name": "John Kasich", "rank": "14","size":100}
+   ]
+  },
+  {
+   "name": "Jim Gilmore",
+   "rank": "15",
+   "size":100,
+   "children": [
+    {"name": "George Pataki", "rank": "1", "size":100},
+    {"name": "Rick Santorum", "rank": "2","size":100},
+    {"name": "Lindsey Graham", "rank": "3","size":100},
+    {"name": "Carly Fiorina", "rank": "4","size":100},
+    {"name": "Rand Paul", "rank": "5","size":100},
+    {"name": "Mike Huckabee", "rank": "6","size":100},
+    {"name": "John Kasich", "rank": "7","size":100},
+    {"name": "Donald Trump","rank": "8", "size":100},
+    {"name": "Ben Carson","rank": "9", "size":100},
+    {"name": "Marco Rubio","rank": "10", "size":100},
+    {"name": "Jeb Bush", "rank": "11","size":100}
+   ]
+  }
+ ]
+}
---
+++ topfive.js
@@ -0,0 +1,108 @@
+var topfive = {
+ "name": "GOP Debate - CNBC",
+ "rank": "",
+ "size":100,
+ "children": [
+  {
+   "name": "Donald Trump",
+   "rank": "1",
+   "size":100,
+   "children": [
+    {"name": "Ben Carson", "rank": "1", "size":100},
+    {"name": "Carly Fiorina", "rank": "2", "size":100},
+    {"name": "Jeb Bush", "rank": "3","size":100},
+    {"name": "Marco Rubio", "rank": "4", "size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie","rank": "7", "size":100},
+    {"name": "Mike Huckabee", "rank": "8","size":100},
+    {"name": "Bobby Jindal", "rank": "9","size":100},
+    {"name": "John Kasich", "rank": "10","size":100},
+    {"name": "Lindsey Graham","rank": "11", "size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+   ]
+  },
+  {
+   "name": "Ben Carson",
+   "rank": "2",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1", "size":100},
+    {"name": "Carly Fiorina","rank": "2", "size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Jeb Bush", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "Mike Huckabee", "rank": "8","size":100},
+    {"name": "John Kasich", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+    
+   ]
+  },
+  {
+   "name": "Carly Fiorina",
+   "rank": "3",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1","size":100},
+    {"name": "Ben Carson", "rank": "2","size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Jeb Bush", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "John Kasich", "rank": "8","size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Lindsey Graham", "rank": "10","size":100},
+    {"name": "Bobby Jindal", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "Jeb Bush",
+   "rank": "4",
+   "size":100,
+   "children": [
+    {"name": "Donald Trump", "rank": "1","size":100},
+    {"name": "Carly Fiorina", "rank": "2","size":100},
+    {"name": "Marco Rubio", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Ted Cruz", "rank": "5","size":100},
+    {"name": "Rand Paul", "rank": "6","size":100},
+    {"name": "Chris Christie", "rank": "7","size":100},
+    {"name": "John Kasich","rank": "8", "size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki", "rank": "13","size":100}
+   ]
+  },
+  {
+   "name": "Marco Rubio",
+   "rank": "5",
+   "size":100,
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100},
+    {"name": "Carly Fiorina","rank": "2", "size":100},
+    {"name": "Donald Trump", "rank": "3","size":100},
+    {"name": "Ben Carson", "rank": "4","size":100},
+    {"name": "Jeb Bush", "rank": "5","size":100},
+    {"name": "Rand Paul","rank": "6", "size":100},
+    {"name": "Chris Christie","rank": "7", "size":100},
+    {"name": "John Kasich","rank": "8", "size":100},
+    {"name": "Mike Huckabee", "rank": "9","size":100},
+    {"name": "Bobby Jindal", "rank": "10","size":100},
+    {"name": "Lindsey Graham", "rank": "11","size":100},
+    {"name": "Rick Santorum", "rank": "12","size":100},
+    {"name": "George Pataki","rank": "13", "size":100}
+   ]
+  }
+  ]
+}
