2e8b52c41871d952ef9674f853966e96513ffc95
Jennifer Lee <leejenn@google.com>
2016-02-27 14:24:14 -0800

    test

---
+++ allcandidates_entire_02.js
@@ -0,0 +1,75 @@
+var allcandidates = {
+ "name": "Super Tuesday",
+ "rank": "",
+ "size": 0,
+ "pic":  "trump.png",
+ "children": [
+  {
+   "name": "Donald Trump",
+   "rank": "1",
+   "size":100,
+   "pic": "trump.png",
+   "children": [
+    {"name": "Ben Carson", "rank": "1", "size":100, "pic":"carson.png"},
+    {"name": "Marco Rubio", "rank": "2","size":100, "pic":"rubio.png"},
+    {"name": "Jeb Bush", "rank": "3","size":100, "pic":"bush.png"},
+    {"name": "Ted Cruz", "rank": "4","size":100, "pic":"cruz.png"},
+    {"name": "Carly Fiorina","rank": "5", "size":100, "pic":"fiorina.png"}
+    
+   ]
+  }, 
+  {
+   "name": "Marco Rubio",
+   "rank": "2",
+   "size":100,
+   "pic": "rubio.png",
+   "children": [
+    {"name": "Donald Trump", "rank": "1", "size":100, "pic":"trump.png"},
+    {"name": "Carly Fiorina","rank": "2", "size":100, "pic":"fiorina.png"},
+    {"name": "Marco Rubio", "rank": "3","size":100, "pic":"rubio.png"},
+    {"name": "Ted Cruz", "rank": "4","size":100, "pic":"cruz.png"},   
+    {"name": "Jeb Bush", "rank": "5","size":100, "pic":"bush.png"}
+    
+   ]
+  },
+  {
+   "name": "Ted Cruz",
+   "rank": "3",
+   "size":100,
+   "pic": "cruz.png",
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100, "pic":"cruz.png"},
+    {"name": "Jeb Bush", "rank": "2","size":100, "pic":"bush.png"},
+    {"name": "Ben Carson", "rank": "3","size":100, "pic":"carson.png"},
+    {"name": "Donald Trump", "rank": "4","size":100, "pic":"trump.png"},
+    {"name": "Carly Fiorina","rank": "5", "size":100, "pic":"fiorina.png"}
+   ]
+  },
+  {
+   "name": "Ben Carson",
+   "rank": "4",
+   "size":100,
+   "pic": "carson.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1","size":100, "pic":"rubio.png"},
+    {"name": "Ben Carson", "rank": "2","size":100, "pic":"carson.png"},
+    {"name": "Donald Trump", "rank": "3","size":100, "pic":"trump.png"},
+    {"name": "Carly Fiorina", "rank": "4","size":100, "pic":"fiorina.png"},
+    {"name": "Jeb Bush", "rank": "5","size":100, "pic":"bush.png"}
+   ]
+  },
+  {
+   "name": "John Kasich",
+   "rank": "5",
+   "size":100,
+   "pic": "kasich.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1", "size":100, "pic":"rubio.png"},
+    {"name": "Donald Trump", "rank": "2", "size":100, "pic":"trump.png"},
+    {"name": "Ted Cruz", "rank": "3","size":100, "pic":"cruz.png"},
+    {"name": "Ben Carson", "rank": "4", "size":100, "pic":"carson.png"},
+    {"name": "Carly Fiorina", "rank": "5","size":100, "pic":"fiorina.png"} 
+   ]
+  }
+ ]
+}
