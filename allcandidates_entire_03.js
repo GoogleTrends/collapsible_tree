6e11c9ab995f1446d5682802a3d85cf249322605
Jennifer Lee <leejenn@google.com>
2016-03-03 13:00:10 -0800

    debate data

---
+++ allcandidates_entire_03.js
@@ -0,0 +1,54 @@
+var allcandidates = {
+ "name": "Republican Debate",
+ "rank": "",
+ "size": 0,
+ "pic":  "trumpv2.png",
+ "children": [
+  {
+   "name": "Donald Trump",
+   "rank": "1",
+   "size":100,
+   "pic": "trumpv2.png",
+   "children": [
+    {"name": "Ted Cruz", "rank": "1", "size":100, "pic":"cruz.png"},
+    {"name": "Marco Rubio", "rank": "2","size":100, "pic":"rubio.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}    
+   ]
+  }, 
+  {
+   "name": "Ted Cruz",
+   "rank": "2",
+   "size":100,
+   "pic": "cruz.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1", "size":100, "pic":"rubio.png"},
+    {"name": "Donald Trump","rank": "2", "size":100, "pic":"trumpv2.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}
+    
+   ]
+  },
+  {
+   "name": "Marco Rubio",
+   "rank": "3",
+   "size":100,
+   "pic": "rubio.png",
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100, "pic":"cruz.png"},
+    {"name": "Donald Trump", "rank": "2","size":100, "pic":"trumpv2.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}
+   ]
+  },
+
+  {
+   "name": "John Kasich",
+   "rank": "4",
+   "size":100,
+   "pic": "kasich.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1", "size":100, "pic":"rubio.png"},
+    {"name": "Ted Cruz", "rank": "2", "size":100, "pic":"cruz.png"},
+    {"name": "Donald Trump", "rank": "3","size":100, "pic":"trumpv2.png"}
+   ]
+  }
+ ]
+}
---
+++ allcandidates_seperate_03.js
@@ -0,0 +1,48 @@
+var trump = {
+   "name": "Donald Trump",
+   "rank": "1",
+   "size":100,
+   "pic": "trumpv2.png",
+   "children": [
+    {"name": "Ted Cruz", "rank": "1", "size":100, "pic":"cruz.png"},
+    {"name": "Marco Rubio", "rank": "2","size":100, "pic":"rubio.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}   
+   ]
+  };
+
+  var cruz = {
+   "name": "Ted Cruz",
+   "rank": "2",
+   "size":100,
+   "pic": "cruz.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1", "size":100, "pic":"rubio.png"},
+    {"name": "Donald Trump","rank": "2", "size":100, "pic":"trumpv2.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}
+    
+   ]
+  };
+
+  var rubio = {
+  "name": "Marco Rubio",
+   "rank": "3",
+   "size":100,
+   "pic": "rubio.png",
+   "children": [
+    {"name": "Ted Cruz", "rank": "1","size":100, "pic":"cruz.png"},
+    {"name": "Donald Trump", "rank": "2","size":100, "pic":"trumpv2.png"},
+    {"name": "John Kasich", "rank": "3","size":100, "pic":"kasich.png"}
+   ]
+  };
+
+  var kasich = {
+   "name": "John Kasich",
+   "rank": "4",
+   "size":100,
+   "pic": "kasich.png",
+   "children": [
+    {"name": "Marco Rubio", "rank": "1", "size":100, "pic":"rubio.png"},
+    {"name": "Ted Cruz", "rank": "2", "size":100, "pic":"cruz.png"},
+    {"name": "Donald Trump", "rank": "3","size":100, "pic":"trumpv2.png"}   
+   ]
+  };
